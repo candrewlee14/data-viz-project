c19f61170d291ccb9fe9abee962d4b16deef83b8
Andrew Lee <candrewlee14@gmail.com>
2022-11-10 11:55:13 -0700

    add country selectors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,12 +2,24 @@
   import * as d3 from "d3";
   import { Location, Product, BilateralTradeYear } from "../models/models";
   import { onMount } from "svelte";
+  import Select from "svelte-select";
   import Example from "../components/Example.svelte";
   import LineChart from "../components/LineChart.svelte";
   import BarChart from "../components/BarChart.svelte";
 
+  const optionIdentifier = "id";
+  const labelIdentifier = "name";
   let country1 = 31;
   let country2 = 15;
+
+  const onSelectCountry1 = (e: any) => {
+    let l = e.detail as Location;
+    country1 = l.id;
+  };
+  const onSelectCountry2 = (e: any) => {
+    let l = e.detail as Location;
+    country2 = l.id;
+  };
 
   $: country1Name = locationData.get(country1)?.name ?? "";
   $: country2Name = locationData.get(country2)?.name ?? "";
@@ -56,7 +68,26 @@
 </script>
 
 <h1>Welcome</h1>
-
+<div class="selectors">
+<div class="dropdown">
+  <span>Country 1</span>
+  <Select
+    {optionIdentifier}
+    {labelIdentifier}
+    items={Array.from(locationData.values())}
+    on:select={onSelectCountry1}
+  />
+</div>
+<div class="dropdown">
+  <span>Country 2</span>
+  <Select
+    {optionIdentifier}
+    {labelIdentifier}
+    items={Array.from(locationData.values())}
+    on:select={onSelectCountry2}
+  />
+</div>
+</div>
 <!-- <Example locationMap={locationData}/> -->
 <div id="container">
   <div id="content">
@@ -100,5 +131,16 @@
     border: 1px solid black;
     margin: 20px;
     padding: 0;
+  }
+  .selectors {
+    width: 100%;
+    display: flex;
+    margin: 0;
+    flex-direction: row;
+    justify-content: center;
+  }
+  .dropdown {
+    margin: 0 10px 0 10px;
+    width: 300px;
   }
 </style>
