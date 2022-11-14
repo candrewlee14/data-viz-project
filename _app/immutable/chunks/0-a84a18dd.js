36a34e610ecf3d6e593de24845de84eb0e069923
candrewlee14 <32912555+candrewlee14@users.noreply.github.com>
2022-11-14 17:54:41 +0000

    Build: (277c05e) update svelte version and fix data props

---
+++ _app/immutable/chunks/0-a84a18dd.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/layout.svelte-c9d4f302.js";export{t as component};
---
+++ _app/immutable/chunks/2-5d43fd18.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-d760b2b9.js";export{t as component};
