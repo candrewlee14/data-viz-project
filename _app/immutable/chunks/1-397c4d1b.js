6f728f2d61d62beec7dcfe66ecea221f07195b2b
fyuan12 <yuan.franklin@gmail.com>
2022-11-16 18:52:02 +0000

    Build: (8a6cd99) update project name

---
+++ _app/immutable/chunks/1-397c4d1b.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-1021d780.js";export{t as component};
--- _app/immutable/chunks/2-d7c41942.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-feace1de.js";export{t as component};
