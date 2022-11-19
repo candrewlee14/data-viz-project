bbf8f18d7922ee6e434fd1118646c9de3cb6c1a4
candrewlee14 <32912555+candrewlee14@users.noreply.github.com>
2022-11-19 09:18:12 +0000

    Build: (28e7377) remove link to favicon from app.html

---
+++ _app/immutable/chunks/1-a3a007d8.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-ddd68bbe.js";export{t as component};
--- _app/immutable/chunks/1-cb575125.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-bedd3b6f.js";export{t as component};
