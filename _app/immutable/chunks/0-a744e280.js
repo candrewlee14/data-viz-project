39d97f2ddaed5df27ce2f1b641c53c9bd74386c8
candrewlee14 <32912555+candrewlee14@users.noreply.github.com>
2022-11-19 09:10:23 +0000

    Build: (92396a4) add svg flags

---
+++ _app/immutable/chunks/0-a744e280.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/layout.svelte-cbe6f481.js";export{t as component};
---
+++ _app/immutable/chunks/1-cb575125.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-bedd3b6f.js";export{t as component};
