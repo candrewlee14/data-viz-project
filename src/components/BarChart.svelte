0b175444172e800ba62eece1ca14edd2c8afed08
Andrew Lee <candrewlee14@gmail.com>
2022-11-18 19:52:53 -0700

    add sorting to bar chart

--- src/components/BarChart.svelte
+++ src/components/BarChart.svelte
@@ -2,13 +2,13 @@
   import * as d3 from "d3";
   import { Location, Product, BilateralTradeYear } from "../models/models";
   import { onMount } from "svelte";
-  import { format, select } from "d3";
-  import {years} from '../stores/store';
+  import { years } from "../stores/store";
   import { fade, fly } from "svelte/transition";
+  import { flip } from "svelte/animate";
 
   export let data: {
-    productData: Map<number, Product>,
-    locationData: Map<number, Location>,
+    productData: Map<number, Product>;
+    locationData: Map<number, Location>;
     bilateralData: Map<number, Map<number, Map<number, BilateralTradeYear[]>>>;
     productColorScale: d3.ScaleOrdinal<string, string, never> | null;
     countryColorScale: d3.ScaleOrdinal<number, string, never>;
@@ -17,8 +17,39 @@
     loadingDrilldown: boolean;
   };
 
-  let {bilateralData, productColorScale, countryColorScale, country1, country2, loadingDrilldown, productData, locationData} = data;
-  $: ({bilateralData, productColorScale, countryColorScale, country1, country2, loadingDrilldown, productData, locationData} = data);
+  enum SortMethod {
+    LeftUp,
+    LeftDown,
+    RightUp,
+    RightDown,
+    Up,
+    Down,
+    AlphaUp,
+    AlphaDown,
+  }
+
+  let sortMethod = SortMethod.AlphaDown;
+
+  let {
+    bilateralData,
+    productColorScale,
+    countryColorScale,
+    country1,
+    country2,
+    loadingDrilldown,
+    productData,
+    locationData,
+  } = data;
+  $: ({
+    bilateralData,
+    productColorScale,
+    countryColorScale,
+    country1,
+    country2,
+    loadingDrilldown,
+    productData,
+    locationData,
+  } = data);
 
   let width = 800;
   let height = 500;
@@ -33,13 +64,20 @@
   const ROW_SPACE = 4;
   const formatter = (val: number) => d3.format("$.2s")(val).replace(/G/, "B");
 
-  $: innerDataByYear = bilateralData?.get(country1?.id ?? 0)
+  $: innerDataByYear = bilateralData
+    ?.get(country1?.id ?? 0)
     ?.get(country2?.id ?? 0);
 
-  $: allInnerDataUnagg = ($years.map((year) => innerDataByYear?.get(year) ?? new Array()).flat() as BilateralTradeYear[]);
+  $: allInnerDataUnagg = $years
+    .map((year) => innerDataByYear?.get(year) ?? new Array())
+    .flat() as BilateralTradeYear[];
 
-  $: innerDataAll = Array.from(d3.rollup(allInnerDataUnagg,
-    (v) => v.reduce(
+  $: innerDataAll = Array.from(
+    d3
+      .rollup(
+        allInnerDataUnagg,
+        (v) =>
+          v.reduce(
             (acc, b) => {
               acc.export_value += b.export_value;
               acc.import_value += b.import_value;
@@ -52,12 +90,43 @@
               year: 0,
               export_value: 0,
               import_value: 0,
-            })),
-    (d) => d.product_id,
-  ).values());
+            })
+          ),
+        (d) => d.product_id
+      )
+      .values()
+  );
 
-  $: tradeData = innerDataAll.filter((v) => v.product.level == "section");
+  $: tradeDataUnsorted = innerDataAll.filter(
+    (v) => v.product.level == "section"
+  );
 
+  $: tradeData = tradeDataUnsorted.sort((a, b) => {
+    switch (sortMethod) {
+      case SortMethod.LeftUp:
+        return a.export_value - b.export_value;
+      case SortMethod.LeftDown:
+        return b.export_value - a.export_value;
+      case SortMethod.RightUp:
+        return a.import_value - b.import_value;
+      case SortMethod.RightDown:
+        return b.import_value - a.import_value;
+      case SortMethod.Up:
+        return (
+          a.import_value + a.export_value - (b.import_value + b.export_value)
+        );
+      case SortMethod.Down:
+        return (
+          b.import_value + b.export_value - (a.import_value + a.export_value)
+        );
+      case SortMethod.AlphaDown:
+        return a.product.name.localeCompare(b.product.name);
+      case SortMethod.AlphaUp:
+        return b.product.name.localeCompare(a.product.name);
+      default:
+        return 0;
+    }
+  });
   // @ts-ignore
   $: maxExport = d3.max(
     innerDataAll,
@@ -92,14 +161,30 @@
 >
   <svg>
     {#if tradeData && tradeData.length > 0}
+      <!-- {#if sortMethod == SortMethod.AlphaUp || sortMethod == SortMethod.AlphaDown}
+        <text x={5} y={LABEL_HEIGHT / 2} alignment-baseline="hanging"
+          class="sort-ind"
+          in:fade={{duration: 100}}
+          out:fade={{duration: 100}}
+          >{sortMethod == SortMethod.AlphaUp ? "↑" : "↓"}</text
+        >
+      {/if} -->
       <text
         text-anchor="start"
         class="chart-header"
         x={15}
         y={LABEL_HEIGHT / 2}
         alignment-baseline="hanging"
+        on:click={() => {
+          if (sortMethod == SortMethod.AlphaDown) {
+            sortMethod = SortMethod.AlphaUp;
+          } else {
+            sortMethod = SortMethod.AlphaDown;
+          }
+        }}
+        on:keydown={() => {}}
       >
-        Sector
+      Sector {sortMethod == SortMethod.AlphaUp ? "↑" : sortMethod == SortMethod.AlphaDown ? "↓" : " "}
       </text>
       <text
         text-anchor="middle"
@@ -107,8 +192,16 @@
         x={(width - PRODUCT_WIDTH) / 2 + PRODUCT_WIDTH}
         y={LABEL_HEIGHT / 2 - 7}
         alignment-baseline="hanging"
+        on:click={() => {
+          if (sortMethod == SortMethod.Down) {
+            sortMethod = SortMethod.Up;
+          } else {
+            sortMethod = SortMethod.Down;
+          }
+        }}
+        on:keydown={() => {}}
       >
-        Trade Flow
+        Trade Flow {sortMethod == SortMethod.Up ? "↑" : sortMethod == SortMethod.Down ? "↓" : " "}
       </text>
       <text
         class="from-label"
@@ -116,8 +209,16 @@
         y={LABEL_HEIGHT / 2 - 5}
         alignment-baseline="hanging"
         font-size="13"
+        on:keydown={() => {}}
+        on:click={() => {
+          if (sortMethod == SortMethod.LeftDown) {
+            sortMethod = SortMethod.LeftUp;
+          } else {
+            sortMethod = SortMethod.LeftDown;
+          }
+        }}
       >
-        From {country1?.name}
+        From {country1?.name + " "}{sortMethod == SortMethod.LeftUp ? "↑" : sortMethod == SortMethod.LeftDown ? "↓" : " "}
       </text>
       <text
         class="from-label"
@@ -126,40 +227,58 @@
         text-anchor="end"
         alignment-baseline="hanging"
         font-size="13"
+        on:click={() => {
+          if (sortMethod == SortMethod.RightDown) {
+            sortMethod = SortMethod.RightUp;
+          } else {
+            sortMethod = SortMethod.RightDown;
+          }
+        }}
       >
-        From {country2?.name}
+      {sortMethod == SortMethod.RightUp ? "↑" : sortMethod == SortMethod.RightDown ? "↓" : " "}&nbsp;From {country2?.name}
       </text>
       <g id="bar-content" transform={`translate(${0},${LABEL_HEIGHT})`}>
-        {#each tradeData as bt, i (bt.product.name)}
-          <g>
+        {#each tradeData as bt, i (bt.product_id)}
+          {#if i % 2 == 0}
             <rect
               x="0"
-              y={i * ROW_HEIGHT + MARGIN_TOP + 2}
+              y={i * ROW_HEIGHT + MARGIN_TOP}
+              height={ROW_HEIGHT}
+              {width}
+              fill="rgba(0,0,0,0.04)"
+            />
+          {/if}
+        {/each}
+        {#each tradeData as bt, i (bt.product_id)}
+          <text
+            class="product-text"
+            alignment-baseline="hanging"
+            x="10"
+            y={i * ROW_HEIGHT + MARGIN_TOP + 6}
+            animate:flip={{ duration: 400 }}>{bt.product.name}</text
+          >
+        {/each}
+        {#each tradeData as bt, i (i)}
+          <g
+            class="bar-row"
+            transform={`translate(0, ${i * ROW_HEIGHT + MARGIN_TOP})`}
+            in:fade
+            out:fade
+          >
+            <rect
+              x="0"
+              y="2"
               height={ROW_HEIGHT - 4}
               width="6"
               fill={productColorScale
                 ? productColorScale(bt.product.name)
                 : "white"}
             />
-            {#if i % 2 == 0}
-              <rect
-                x="0"
-                y={i * ROW_HEIGHT + MARGIN_TOP}
-                height={ROW_HEIGHT}
-                {width}
-                fill="rgba(0,0,0,0.04)"
-              />
-            {/if}
-            <text
-              class="product-text"
-              alignment-baseline="hanging"
-              x={10}
-              y={i * ROW_HEIGHT + MARGIN_TOP + 5}>{bt.product.name}</text
-            >
+
             <rect
               class="country1-bar"
               x={barScale(-bt.export_value)}
-              y={i * ROW_HEIGHT + MARGIN_TOP + ROW_SPACE / 2}
+              y={ROW_SPACE / 2}
               height={ROW_HEIGHT - ROW_SPACE}
               width={barScale(0) - barScale(-bt.export_value)}
               fill={countryColorScale(bt.location_id)}
@@ -167,7 +286,7 @@
             <rect
               class="country2-bar"
               x={barScale(0)}
-              y={i * ROW_HEIGHT + MARGIN_TOP + ROW_SPACE / 2}
+              y={ROW_SPACE / 2}
               height={ROW_HEIGHT - ROW_SPACE}
               width={barScale(bt.import_value) - barScale(0)}
               fill={countryColorScale(bt.partner_id)}
@@ -202,8 +321,13 @@
       >
     {/if}
     {#if loadingDrilldown}
-      <rect x=0 y=0 width={width} height={height} fill="rgba(255,255,255,0.9)"/>
-      <text x={width/2} y={height/2} alignment-baseline="central" text-anchor="middle">Loading...</text>
+      <rect x="0" y="0" {width} {height} fill="rgba(255,255,255,0.9)" />
+      <text
+        x={width / 2}
+        y={height / 2}
+        alignment-baseline="central"
+        text-anchor="middle">Loading...</text
+      >
     {/if}
   </svg>
 </div>
