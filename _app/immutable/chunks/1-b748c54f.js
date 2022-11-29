f0c328b066858c72a9cf73dc7d4cdc2f609d056c
fyuan12 <yuan.franklin@gmail.com>
2022-11-29 06:33:56 +0000

    Build: (2754282) modify certain wording

--- _app/immutable/chunks/1-b748c54f.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-f33ee399.js";export{t as component};
--- _app/immutable/chunks/2-dce454a3.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-da59d998.js";export{t as component};
