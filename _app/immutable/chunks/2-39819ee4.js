5d6e3579044634700d4b60fdf60e105c6cd8ade8
fyuan12 <yuan.franklin@gmail.com>
2022-11-30 03:57:59 +0000

    Build: (b8dee85) fix a bug on line chart

--- _app/immutable/chunks/2-39819ee4.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-011312f4.js";export{t as component};
---
+++ _app/immutable/chunks/2-c522b5ff.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-f0d05267.js";export{t as component};
