3cb85a88230e8342ce6744551d22160878604e40
fyuan12 <yuan.franklin@gmail.com>
2022-11-30 16:49:53 +0000

    Build: (deefb26) set two main colors for countries and more icons

---
+++ _app/immutable/chunks/1-57ddc7aa.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-91fbe487.js";export{t as component};
--- _app/immutable/chunks/2-d7d31463.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-22a1ce4d.js";export{t as component};
