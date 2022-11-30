01329ab373f1a90de57c44b9234c8465bf3fbfc4
fyuan12 <yuan.franklin@gmail.com>
2022-11-30 17:12:44 +0000

    Build: (cb8d685) add year in bar chart title

---
+++ _app/immutable/chunks/1-6d024695.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-a4aa4c57.js";export{t as component};
---
+++ _app/immutable/chunks/2-f923c1de.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-7d3b2b73.js";export{t as component};
