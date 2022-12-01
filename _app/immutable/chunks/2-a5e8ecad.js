1380c7010023efc458ee4c027f317a44d4932468
fyuan12 <yuan.franklin@gmail.com>
2022-12-01 18:32:22 +0000

    Build: (25e1207) add axis labels and fix font

---
+++ _app/immutable/chunks/2-a5e8ecad.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-0fa1ac90.js";export{t as component};
--- _app/immutable/chunks/2-faf0c071.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-c4db29b1.js";export{t as component};
