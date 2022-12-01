34582a20ec8096f0d785a59918385e9a980db8aa
fyuan12 <yuan.franklin@gmail.com>
2022-12-01 23:55:34 +0000

    Build: (ad1c14e) fix google font link issue

--- _app/immutable/chunks/2-49950c77.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-327b1868.js";export{t as component};
---
+++ _app/immutable/chunks/2-aaa8da04.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-3c0ec9ea.js";export{t as component};
