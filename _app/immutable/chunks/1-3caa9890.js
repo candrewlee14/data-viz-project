235bf46afe9f28c3e2ffc3919a20ae50058f7332
fyuan12 <yuan.franklin@gmail.com>
2022-12-02 05:05:34 +0000

    Build: (d4bcf48) Change project structure and update README

--- _app/immutable/chunks/1-3caa9890.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-db8383b7.js";export{t as component};
---
+++ _app/immutable/chunks/1-42b04b6f.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-c21c1ddd.js";export{t as component};
