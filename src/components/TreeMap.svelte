086e47a7082eaf462561690218229a481dbc747b
Andrew Lee <candrewlee14@gmail.com>
2022-12-01 19:55:45 -0700

    fix treemap ellipsis and refactor tooltip

--- src/components/TreeMap.svelte
+++ src/components/TreeMap.svelte
@@ -48,6 +48,27 @@
   const TEXT_OFFSET_Y_BASE = 28;
   const TEXT_OFFSET_Y_INCRE = 15;
 
+  let tooltipLayerX = 0;
+  let tooltipLayerY = 0;
+  let tooltipData: BilateralTradeYear | null = null;
+
+  $: tooltipText = [
+    `${tooltipData?.product.name}`,
+    `Sector: ${tooltipData?.product?.parent?.name ?? ""}`,
+    ($showExport ? "Export: " : "Import: ") +
+      `${formatter(
+        $showExport
+          ? tooltipData?.export_value ?? 0
+          : tooltipData?.import_value ?? 0
+      )}`,
+    `Share: ${d3.format(".2%")(
+      $showExport
+        ? (tooltipData?.export_value ?? 0) / sumExport
+        : (tooltipData?.import_value ?? 0) / sumImport
+    )}`,
+    ($years.length > 1 ? "Years: " : "Year: ") + `${$years.join(", ")}`,
+  ];
+
   $: innerDrilldownBilateral = drilldownBilateral?.get(country2?.id ?? 0);
 
   $: allInnerDataUnagg = $years
@@ -200,7 +221,6 @@
     }
 
     bls = bls.concat(bilaterals);
-    // // console.log(bls);
 
     let treemapRoot = d3
       .stratify()
@@ -236,138 +256,16 @@
     }
   }
 
-  function mouseOver(bt: BilateralTradeYear): (e: any) => void {
+  function mouseUpdate(bt: BilateralTradeYear): (e: any) => void {
     return (e: any) => {
-      let tooltip = d3.select("#treemap-tooltip");
-      let tooltipWidth = Math.max(
-        TOOLTIP_RECT_WIDTH_BASE + $years.length * TOOLTIP_RECT_WIDTH_INCRE,
-        TOOLTIP_RECT_WIDTH_BASE + 10 + (bt.product.name.length - 20) * 8
-      );
-
-      tooltip
-        .append("rect")
-        .attr("id", "tooltip-rect")
-        .attr("width", tooltipWidth)
-        .attr("height", TOOLTIP_RECT_HEIGHT)
-        .attr("x", getTooltipX(e.layerX, TOOLTIP_OFFSET, tooltipWidth))
-        .attr("y", getTooltipY(e.layerY, TOOLTIP_OFFSET));
-
-      // Product
-      tooltip
-        .append("text")
-        .attr("class", "treemap")
-        .attr("id", "tooltip-text1")
-        .attr("x", getTooltipX(e.layerX, TEXT_OFFSET_X, tooltipWidth))
-        .attr("y", getTooltipY(e.layerY, TEXT_OFFSET_Y_BASE))
-        .style("font-weight", "bold")
-        .text(`${bt.product.name}`);
-
-      // Sector
-      tooltip
-        .append("text")
-        .attr("class", "treemap")
-        .attr("id", "tooltip-text2")
-        .attr("x", getTooltipX(e.layerX, TEXT_OFFSET_X, tooltipWidth))
-        .attr(
-          "y",
-          getTooltipY(e.layerY, TEXT_OFFSET_Y_BASE + TEXT_OFFSET_Y_INCRE)
-        )
-        .text(`Sector: ${bt.product?.parent?.name ?? ""}`);
-
-      // Export/Import
-      tooltip
-        .append("text")
-        .attr("class", "treemap")
-        .attr("id", "tooltip-text3")
-        .attr("x", getTooltipX(e.layerX, TEXT_OFFSET_X, tooltipWidth))
-        .attr(
-          "y",
-          getTooltipY(e.layerY, TEXT_OFFSET_Y_BASE + 2 * TEXT_OFFSET_Y_INCRE)
-        )
-        .text(
-          ($showExport ? "Export: " : "Import: ") +
-            `${formatter($showExport ? bt.export_value : bt.import_value)}`
-        );
-
-      // Share
-      tooltip
-        .append("text")
-        .attr("class", "treemap")
-        .attr("id", "tooltip-text4")
-        .attr("x", getTooltipX(e.layerX, TEXT_OFFSET_X, tooltipWidth))
-        .attr(
-          "y",
-          getTooltipY(e.layerY, TEXT_OFFSET_Y_BASE + 3 * TEXT_OFFSET_Y_INCRE)
-        )
-        .text(
-          `Share: ${d3.format(".2%")(
-            $showExport
-              ? bt.export_value / sumExport
-              : bt.import_value / sumImport
-          )}`
-        );
-
-      // Years
-      tooltip
-        .append("text")
-        .attr("class", "treemap")
-        .attr("id", "tooltip-text5")
-        .attr("x", getTooltipX(e.layerX, TEXT_OFFSET_X, tooltipWidth))
-        .attr(
-          "y",
-          getTooltipY(e.layerY, TEXT_OFFSET_Y_BASE + 4 * TEXT_OFFSET_Y_INCRE)
-        )
-        .text(
-          ($years.length > 1 ? "Years: " : "Year: ") + `${$years.join(", ")}`
-        );
+      tooltipLayerX = e.layerX;
+      tooltipLayerY = e.layerY;
+      tooltipData = bt;
     };
   }
 
-  function mouseMove(bt: BilateralTradeYear): (e: any) => void {
-    return (e: any) => {
-      let tooltipWidth = Math.max(
-        TOOLTIP_RECT_WIDTH_BASE + $years.length * TOOLTIP_RECT_WIDTH_INCRE,
-        TOOLTIP_RECT_WIDTH_BASE + 10 + (bt.product.name.length - 20) * 8
-      );
-
-      d3.select("#tooltip-rect")
-        .attr("x", getTooltipX(e.layerX, TOOLTIP_OFFSET, tooltipWidth))
-        .attr("y", getTooltipY(e.layerY, TOOLTIP_OFFSET));
-      d3.select("#tooltip-text1")
-        .attr("x", getTooltipX(e.layerX, TEXT_OFFSET_X, tooltipWidth))
-        .attr("y", getTooltipY(e.layerY, TEXT_OFFSET_Y_BASE));
-      d3.select("#tooltip-text2")
-        .attr("x", getTooltipX(e.layerX, TEXT_OFFSET_X, tooltipWidth))
-        .attr(
-          "y",
-          getTooltipY(e.layerY, TEXT_OFFSET_Y_BASE + TEXT_OFFSET_Y_INCRE)
-        );
-      d3.select("#tooltip-text3")
-        .attr("x", getTooltipX(e.layerX, TEXT_OFFSET_X, tooltipWidth))
-        .attr(
-          "y",
-          getTooltipY(e.layerY, TEXT_OFFSET_Y_BASE + 2 * TEXT_OFFSET_Y_INCRE)
-        );
-      d3.select("#tooltip-text4")
-        .attr("x", getTooltipX(e.layerX, TEXT_OFFSET_X, tooltipWidth))
-        .attr(
-          "y",
-          getTooltipY(e.layerY, TEXT_OFFSET_Y_BASE + 3 * TEXT_OFFSET_Y_INCRE)
-        );
-      d3.select("#tooltip-text5")
-        .attr("x", getTooltipX(e.layerX, TEXT_OFFSET_X, tooltipWidth))
-        .attr(
-          "y",
-          getTooltipY(e.layerY, TEXT_OFFSET_Y_BASE + 4 * TEXT_OFFSET_Y_INCRE)
-        );
-    };
-  }
-
-  function mouseLeave(): (e: any) => void {
-    return (e: any) => {
-      d3.select("#treemap-tooltip").selectAll("rect").remove();
-      d3.select("#treemap-tooltip").selectAll("text").remove();
-    };
+  function mouseLeave() {
+    tooltipData = null;
   }
 
   function onClick(bt: BilateralTradeYear): (e: any) => void {
@@ -375,13 +273,11 @@
       if (bt?.product?.parent && $sectors.has(bt?.product?.parent?.id)) {
         sectors.update((s) => {
           s.delete(bt?.product?.parent?.id ?? -1);
-          // console.log(s);
           return s;
         });
       } else if (bt?.product?.parent != null) {
         sectors.update((s) => {
           s.add(bt?.product?.parent?.id ?? -1);
-          // console.log(s);
           return s;
         });
       }
@@ -402,7 +298,7 @@
             class="treemap-inner"
             transform={`translate(${leaf.x0},${leaf.y0})`}
           >
-            {#if (leaf.x1 - leaf.x0) > 0 && (leaf.y1 - leaf.y0) > 0}
+            {#if leaf.x1 - leaf.x0 > 0 && leaf.y1 - leaf.y0 > 0}
               <rect
                 class="leaf"
                 width={leaf.x1 - leaf.x0}
@@ -412,66 +308,75 @@
                   : "white"}
                 on:keydown={() => {}}
                 on:click={onClick(leaf.data)}
-                on:focus
-                on:mouseover={mouseOver(leaf.data)}
-                on:mousemove={mouseMove(leaf.data)}
-                on:mouseleave={mouseLeave()}
+                on:mouseover={mouseUpdate(leaf.data)}
+                on:mousemove={mouseUpdate(leaf.data)}
+                on:mouseleave={mouseLeave}
               />
             {/if}
-            {#if (leaf.x1 - leaf.x0) > 3 && (leaf.y1 - leaf.y0) > 0}
-              <clipPath id={`${uid}-clip-${i}`}>
-                <rect
-                  width={leaf.x1 - leaf.x0 - 3}
-                  height={leaf.y1 - leaf.y0}
-                />
-              </clipPath>
-            {/if}
-            {#if leaf.y1 - leaf.y0 > 15}
-              <text
-                class="label"
-                clip-path={`url(${new URL(
-                  `#${uid}-clip-${i}`,
-                  window.location.href
-                )})`}
-                transform={`translate(4,5)`}
-                alignment-baseline="hanging"
+            {#if leaf.y1 - leaf.y0 > 12 && leaf.x1 - leaf.x0 > 10}
+              <foreignObject
+                x="3"
+                y="1"
+                width={leaf.x1 - leaf.x0 - 3}
+                height={leaf.y1 - leaf.y0 - 1}
                 on:keydown={() => {}}
                 on:click={onClick(leaf.data)}
-                on:focus
-                on:mouseover={mouseOver(leaf.data)}
-                on:mousemove={mouseMove(leaf.data)}
-                on:mouseleave={mouseLeave()}
+                on:mouseover={mouseUpdate(leaf.data)}
+                on:mousemove={mouseUpdate(leaf.data)}
+                on:mouseleave={mouseLeave}
               >
-                {leaf.data?.product.name?.substring(0, 40) ?? ""}
-              </text>
-            {/if}
-            {#if leaf.y1 - leaf.y0 > 35}
-              <text
-                class="label-num"
-                clip-path={`url(${new URL(
-                  `#${uid}-clip-${i}`,
-                  window.location.href
-                )})`}
-                transform={`translate(4,${23})`}
-                alignment-baseline="hanging"
-                font-weight="300"
-                on:keydown={() => {}}
-                on:click={onClick(leaf.data)}
-                on:focus
-                on:mouseover={mouseOver(leaf.data)}
-                on:mousemove={mouseMove(leaf.data)}
-                on:mouseleave={mouseLeave()}
-              >
-                {formatter(
-                  $showExport ? leaf.data.export_value : leaf.data.import_value
-                )}
-              </text>
+                <div
+                  class="label"
+                  on:keydown={() => {}}
+                  on:click={onClick(leaf.data)}
+                  on:mouseover={mouseUpdate(leaf.data)}
+                  on:mousemove={mouseUpdate(leaf.data)}
+                  on:mouseleave={mouseLeave}
+                >
+                  {leaf.data?.product.name ?? ""}
+                </div>
+                <div class="label-num">
+                  {formatter(
+                    $showExport
+                      ? leaf.data.export_value
+                      : leaf.data.import_value
+                  )}
+                </div>
+              </foreignObject>
             {/if}
           </g>
         {/if}
       {/each}
     </g>
-    <g id="treemap-tooltip" class="tooltip" />
+    {#if tooltipData}
+      {@const tooltipWidth = Math.max(
+        TOOLTIP_RECT_WIDTH_BASE + $years.length * TOOLTIP_RECT_WIDTH_INCRE,
+        TOOLTIP_RECT_WIDTH_BASE +
+          10 +
+          (tooltipData.product.name.length - 20) * 8
+      )}
+      <g id="treemap-tooltip" class="tooltip">
+        <rect
+          id="tooltip-rect"
+          x={getTooltipX(tooltipLayerX, TOOLTIP_OFFSET, tooltipWidth)}
+          y={getTooltipY(tooltipLayerY, TOOLTIP_OFFSET)}
+          width={tooltipWidth}
+          height={TOOLTIP_RECT_HEIGHT}
+        />
+        {#each tooltipText as text, i}
+          <text
+            x={getTooltipX(tooltipLayerX, TEXT_OFFSET_X, tooltipWidth)}
+            y={getTooltipY(
+              tooltipLayerY,
+              TEXT_OFFSET_Y_BASE + i * TEXT_OFFSET_Y_INCRE
+            )}
+            font-weight={i == 0 ? "bold" : "regular"}
+          >
+            {text}
+          </text>
+        {/each}
+      </g>
+    {/if}
     {#if (bilaterals?.length ?? 0) === 0}
       <rect
         x="0"
@@ -501,7 +406,9 @@
     height: 100%;
     margin: 0;
   }
-  .treemap text {
+  .treemap text,
+  div,
+  .tooltip text {
     transition: all 0.4s ease;
     font-size: 12px;
   }
@@ -510,5 +417,19 @@
   }
   .treemap rect {
     transition: all 0.4s ease;
+    cursor: pointer;
+  }
+  .treemap foreignObject {
+    cursor: pointer;
+  }
+  .label-num {
+    font-weight: 300;
+  }
+  .label,
+  .label-num {
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    cursor: pointer;
   }
 </style>
